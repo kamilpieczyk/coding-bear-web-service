cb96abb618105d8d3c6c3438cceb206f044142ba
Kamil <kamil@coding-bear.co.uk>
2019-06-19 14:58:06 +0100

    Add backend site
    Add content of backend site, and changes in css in solution file. Add opacity effect after scroll in solutions

--- pages/solutions.js
+++ pages/solutions.js
@@ -14,6 +14,7 @@ const Container = styled.section`
     width: 100vw;
     height: 100vh;
     background: linear-gradient(130deg, ${colors.main}, ${colors.third});
+    opacity: ${ props => props.opacity ? props.opacity : 1 };
     display: flex;
     flex-direction: row;
     justify-content: space-around;
@@ -45,6 +46,12 @@ const Front = styled.div`
 const Contnet = styled.article`
     margin: 10px 10vw;
     color: ${colors.grey};
+    img{
+        margin: 20px 0 20px 0;
+        margin-left: 50%;
+        transform: translateX(-50%);
+        width: 300px;
+    }
 `;
 
 const TitleFront = styled.h1`
@@ -52,13 +59,35 @@ const TitleFront = styled.h1`
 
 `;
 
-const Solutions = ({router}) => {
-    const [loaded, setLoaded] = useState(false);
+const Solutions = ({ router }) => {
+    const  [loaded, setLoaded ] = useState(false);
+    const [ opacity, setOpacity ] = useState(1);
 
     const content = data(router.query.name);
 
+    let oldScroll;
+
+    const handleScrollEffect = (e) => {
+        const scroll = window.scrollY;
+        const height = window.innerHeight;
+        const percent = scroll / height;
+        const opacityDecrement = 1 - percent.toFixed(1) - 0.2;
+        const opacityIncrement = 0 + opacityDecrement + 0.2;
+
+        if(scroll > oldScroll){
+            oldScroll = scroll;
+            setOpacity( opacityDecrement )
+        }
+        else if(scroll < oldScroll){
+            oldScroll = scroll;
+            setOpacity( opacityIncrement )
+        }
+    }
+
     useEffect(() => {
-        document.addEventListener('load', () => setLoaded(true));
+        document.addEventListener('load', () => setLoaded(true) );
+        oldScroll = window.scrollY;
+        document.addEventListener('scroll', (e) => handleScrollEffect(e) );
     });
 
     const handleLoading = ( callback ) => {
@@ -84,7 +113,7 @@ const Solutions = ({router}) => {
             </Head>
 
             <MainLayout>
-                <Container>
+                <Container opacity = { opacity } >
                     <img src={content.image} alt="solutions-image" />
                     <Front>
                         <TitleFront>{content.title}</TitleFront>
@@ -99,6 +128,7 @@ const Solutions = ({router}) => {
                         <Contnet>
                             <h2>{article.title}</h2>
                             <div>{article.content}</div>
+                            <img src={article.image} />
                         </Contnet>
                     ))
                 }</Wrapper>
@@ -133,7 +163,7 @@ function data(name) {
             content: [
                 {
                     title: "Powerfull react.js",
-                    content: "React.js is a javascript framework which has been made by facebook developers for faster, easier and much more safty coding. Thanks to react web-developers are able to build applications better. The main feature of react is virtual document-object-model. React stores the entire DOM of the application in memory, after changing the state searches for differences between the virtual and real DOM and updates the changes. \n The framework is very powerfull. A lot of famous brand like facebook, netflix, pinterest, instagram, etc. are using react - whole have been build in it. If you would like to see an example - just check facebook.com - this is react."
+                    content: "React.js is a javascript framework which has been made by facebook developers. Thanks to react web devs are able to build applications better and faster than in common java-script. The main feature of react is virtual document-object-model. React stores the entire DOM of the application in memory, after changing the state searches for differences between the virtual and real DOM and updates the changes. \n The framework is very powerfull. A lot of famous brand like facebook, netflix, pinterest, instagram, etc. are using react - whole have been build in it. If you would like to see an example - just check facebook.com - this is react."
                 },
                 {
                     title: "Very light vue.js",
@@ -145,15 +175,20 @@ function data(name) {
         backend: {
             title: "Backend programming",
             image: "/static/images/backend.png",
-            description: "Nowadays many popular javascript frameworks are used for developing modern web applications. Coding-bear usually works with two: react.js for bigger projects and vue.js for small projects",
+            description: "Maybe you don't know but backend is the most important part of. This is all of the logic of your website / application - everything what is going on under view layer.",
             content: [
                 {
-                    title: "Powerfull react.js",
-                    content: "React.js is a javascript framework which has been made by facebook developers for faster, easier and much more safty coding. Thanks to react web-developers are able to build applications better. The main feature of react is virtual document-object-model. React stores the entire DOM of the application in memory, after changing the state searches for differences between the virtual and real DOM and updates the changes. \n The framework is very powerfull. A lot of famous brand like facebook, netflix, pinterest, instagram, etc. are using react - whole have been build in it. If you would like to see an example - just check facebook.com - this is react."
+                    title: "How backend works?",
+                    content: `Everythink what you can see on your website / webapp are client (frontend) - your view. Backend is everything what you can't see but doing the biggest part of job - is all of the project's logic. \n When you add a new post on you blog you actually send a POST query from your client to the backend. Then backend handle your query and add your new post to database. Then your client is able to send another query (GET) if someone would like to read your new blog post and as a response backend will send content of the post. This is how it works in short. The picture below visualize difference between backend and frontend the best.`,
+                    image:"static/images/backend.jpg"
                 },
                 {
-                    title: "Very light vue.js",
-                    content: "Vue.js is exacly like react a JavaScript framework for building user interfaces and single-page applications. Vue is an open-source solution created by Evan You and realised first time in 2014. Now in 2019 vue is very fast and light."
+                    title: "Our approach to backend",
+                    content: "Java-script is the most popular programming language since many years. It is like that because it's just one of the most intuitive, modern and practical. \n On the beginning javascript was rendered only browser side - that was time when imperfect PHP reigned on server side. Thanks to one of the best developers across the world - Ryan Dahl since 2009 we can run javascript everywhere. He has created node.js - the runtime for javascript out of the browser. To do this he used v8 engine by google - the best js interpreter. Novadays, developers are focused to improve one language, not like formerly -one for frontend, one for backend and another for differend things. Coding-bear's working with both of the languages full-stack javascript and PHP. Node.js with pleasure - PHP if it is absolutley required by customer."
+                },
+                {
+                    title: "Databeses",
+                    content: "Nowadays a lot of companies choose MongoDB, because its non SQL database - not like MySQL or PostgreSQL. This approach has a lot of benefits and a lot of defects. SQL databases are exposed for SQL injection attacks - but they have strong relations between records. Non SQL databases haven't relations but they don't use SQL language. Which one is the best? There is not a clear ansver for this question. Everything depends. Usually MongoDb is just enough for most projects but sometimes applications are very expand and needs relations beetween records. Coding-bear usually recomands MongoDB"
                 },
             ]
         },
